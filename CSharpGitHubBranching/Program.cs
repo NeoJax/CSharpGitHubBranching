ee1d139680ded373d0d8e45b4f9eed71036bf349
Johnathon Lubaway <NeoJax@gmail.com>
2019-10-31 10:21:27 -0400

    testing

--- CSharpGitHubBranching/Program.cs
+++ CSharpGitHubBranching/Program.cs
@@ -6,7 +6,7 @@ namespace CSharpGitHubBranching
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello People.");
         }
     }
 }
